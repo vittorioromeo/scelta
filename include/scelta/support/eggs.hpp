bba225939a5ca3437b09b873dcd9a3b21e51a36b
Vittorio Romeo <vittorio.romeo@outlook.com>
2017-04-16 00:17:03 +0100

    Formatting

--- include/scelta/support/eggs.hpp
+++ include/scelta/support/eggs.hpp
@@ -5,8 +5,10 @@
 
 #pragma once
 
+// clang-format off
 // Usage of C++17: `__has_include`.
 #if __has_include(<eggs/variant.hpp>)
+// clang-format on
 
 #include "../utils/variant_homogenizer.hpp"
 #include <eggs/variant.hpp>
