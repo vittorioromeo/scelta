31ee67115cafaf74bc1d75ae500f2ef2fea2ab4d
Vittorio Romeo <vittorio.romeo@outlook.com>
2017-04-29 16:24:01 +0100

    WIP replace_all with CVs

--- include/scelta/meta/replace_all.hpp
+++ include/scelta/meta/replace_all.hpp
@@ -11,6 +11,50 @@
 // Usage of C++17: nested `namespace`.
 namespace scelta::meta
 {
+    namespace impl
+    {
+        template <typename T>
+        using identity = T;
+
+        template <typename, typename T>
+        using dont_transform = T;
+
+        template <template <typename> typename OnCheck,
+            template <typename, typename> typename OnReplace, typename Before,
+            typename After, typename T>
+        struct apply : std::conditional<std::is_same_v<OnCheck<T>, Before>,
+                           OnReplace<T, After>, T>
+        {
+        };
+
+        template <template <typename> typename OnCheck,
+            template <typename, typename> typename OnReplace, typename Before,
+            typename After, typename T>
+        struct apply<OnCheck, OnReplace, Before, After, T<Ts...>>
+        {
+            using type = T<
+                typename apply<OnCheck, OnReplace, Before, After, Ts>::type...>;
+        };
+
+
+
+        template <template <typename> typename OnCheck,
+            template <typename, typename> typename OnReplace>
+        struct replace_all_impl
+        {
+            template <typename Before, typename After, typename T>
+            using apply_t =
+                typename apply<OnCheck, OnReplace, Before, After, T>::type;
+        };
+
+        using noop_replacer = replace_all_impl<identity, dont_transform>;
+        using cvrefptr_replacer = replace_all_impl<remove_cv_ref_ptr_t, copy_cv_ptr_ref_t>;
+    }
+
+
+
+/*
+
     template <typename Before, typename After, typename T>
     struct replace_all : std::conditional<std::is_same_v<T, Before>, After, T>
     {
@@ -48,5 +92,5 @@ namespace scelta::meta
         {
             using type = T<ra_cv_t<Before, After, Ts>...>;
         };
-    }
+    }*/
 }
