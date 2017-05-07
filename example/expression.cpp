ba0a2173c85105bae150c35ab8313c6f13840b94
Vittorio Romeo <vittorio.romeo@outlook.com>
2017-05-07 22:35:08 +0100

    Added expression evaluation example

--- example/expression.cpp
+++ example/expression.cpp
@@ -26,23 +26,45 @@ namespace impl
     using _ = sr::placeholder;
 
     template <typename T>
-    using recr_expr = std::tuple<num, op, std::unique_ptr<_>>;
+    using recr_expr = std::tuple<num, op, _>;
 
-    using builder = sr::builder<example::variant<num, recr_expr<_>>>;
+    using builder =
+        sr::builder<example::variant<num, std::unique_ptr<recr_expr<_>>>>;
 
     using type = sr::type<builder>;
     using resolved_recr_expr = sr::resolve<builder, recr_expr<_>>;
 }
 
-using base_expr = impl::type;
+using expr = impl::type;
 using recr_expr = impl::resolved_recr_expr;
 
 template <typename Op, typename Rest>
-auto recr(num x, Op op, Rest&& rest)
+auto recr(num x, Op o, Rest&& rest)
 {
-    return std::make_unique<recr_expr>(x, op, FWD(rest));
+    return std::make_unique<recr_expr>(x, op{o}, FWD(rest));
 }
 
 int main()
 {
+    expr e = recr(2, o_mul{}, recr(5, o_add{}, recr(15, o_div{}, 3)));
+
+    // clang-format off
+    std::cout << scelta::recursive::match<num>(
+        [](auto, const num& x) 
+        { 
+            return x; 
+        },
+        [](auto recurse, const std::unique_ptr<recr_expr>& x) 
+        {
+            const auto& lhs = std::get<0>(*x);
+            const auto& op  = std::get<1>(*x);
+            const auto& rhs = recurse(std::get<2>(*x));
+
+            return scelta::match([&](o_add){ return lhs + rhs; },
+                                 [&](o_sub){ return lhs - rhs; },
+                                 [&](o_mul){ return lhs * rhs; },
+                                 [&](o_div){ return lhs / rhs; })(op);
+        })
+        (e);
+    // clang-format on
 }
