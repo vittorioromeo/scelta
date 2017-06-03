e799da16bc9d2e31cb845cf7cf5b963402467acf
Vittorio Romeo <vittorio.romeo@outlook.com>
2017-06-03 11:35:34 +0100

    conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, tools
+from conans import ConanFile, tools, CMake
 import os
 
 class SceltaConan(ConanFile):
@@ -6,10 +6,19 @@ class SceltaConan(ConanFile):
     version = "0.1"
     url = "https://github.com/SuperV1234/scelta.git"
     build_policy = "missing"
+    settings = "os", "compiler", "build_type", "arch"
 
     def source(self):
         self.run("git clone https://github.com/SuperV1234/scelta.git")
         self.run("cd scelta && git checkout v0.1 && git submodule update --init")
 
+    def build(self):
+        cmake = CMake(self)
+        self.run('cmake %s %s' % (self.source_folder, cmake.command_line))
+        self.run("cmake --build . %s" % cmake.build_config)
+
     def package(self):
         self.copy("*.hpp", dst="include", src="scelta/include")
+
+    def package_info(self):
+        self.info.header_only()
