f216b95945d1d9f5f740ad18253b7d31eb535716
Vittorio Romeo <vittorio.romeo@outlook.com>
2018-03-15 21:31:12 +0000

    Fix ADL lookup

--- include/scelta/utils/optional_monadic_infix.hpp
+++ include/scelta/utils/optional_monadic_infix.hpp
@@ -13,26 +13,25 @@ namespace scelta::infix
         template <typename F>
         struct infix_f : F
         {
-            constexpr infix_f(F&& f) noexcept(noexcept(F{FWD(f)}))
-                : F{FWD(f)}
+            constexpr infix_f(F&& f) noexcept(noexcept(F{FWD(f)})) : F{FWD(f)}
             {
             }
         };
+
+        // clang-format off
+        template <typename Optional, typename F>
+        constexpr auto operator|(Optional&& o, infix_f<F>&& f)
+            SCELTA_RETURNS(
+               f(FWD(o))
+            )
+        // clang-format on
     }
 
-    // clang-format off
-    template <typename Optional, typename F>
-    constexpr auto operator|(Optional&& o, detail::infix_f<F>&& f)
-        SCELTA_RETURNS(
-           f(FWD(o))
-        )
-    // clang-format on
 
 #define SCELTA_DEFINE_INFIX(name)                                              \
     template <typename... Ts>                                                  \
     constexpr auto name(Ts&&... xs)                                            \
     {                                                                          \
-                                                                               \
         return detail::infix_f{                                                \
             [&](auto&& o) SCELTA_RETURNS(::scelta::name(FWD(o), FWD(xs)...))}; \
     }
